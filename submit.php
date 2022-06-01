d043ce2b3d68b3ff55e80c91c1530569d0319bbf
Blackie246810 <106505448+Blackie246810@users.noreply.github.com>
2022-06-01 11:21:25 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>
+Form
+</title>
+</head>
+<body>
+<script>
+document.getElementById("connect").innerHTML = "Submission failed"
+document.getElementById("connect").style.color = "red"
+</script>
+</body>
+</html>
